1401332b36113169dfe9fe9c90d8ae2e72192a4a
Frontasts <steamfronts0110@gmail.com>
2026-08-12 22:36:09 +0300

    Добавлен класс CryptoProcessor требующийся по условиям задачи

---
+++ HomeWorckPaymentGateway/CryptoProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckPaymentGateway
+{
+    public class CryptoProcessor : PaymentProcessor
+    {
+        private string _cryptoType;
+        private decimal _exchangeRate;
+        private decimal _networkFee;
+
+        public CryptoProcessor(string name, decimal feePercent, string cryptoType, decimal exchangeRate, decimal networkFee) : base(name, feePercent)
+        {
+            _cryptoType = cryptoType;
+            _exchangeRate = exchangeRate;
+            _networkFee = networkFee;
+        }
+    }
+}
