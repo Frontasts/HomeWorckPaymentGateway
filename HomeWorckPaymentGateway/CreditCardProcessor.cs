f5b8ea829f8f4622d498c2dc9b21acd68268eadf
Frontasts <steamfronts0110@gmail.com>
2026-08-12 22:24:12 +0300

    Добавлен класс CreditCardProcessor

---
+++ HomeWorckPaymentGateway/CreditCardProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckPaymentGateway
+{
+    public class CreditCardProcessor : PaymentProcessor
+    {
+        public CreditCardProcessor(string name, decimal feePercent): base(name, feePercent)
+        {
+        }
+    }
+}
