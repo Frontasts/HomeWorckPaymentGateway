70fa3647074d3bfe755a5005dc09aeee96c3895e
Frontasts <steamfronts0110@gmail.com>
2026-08-12 22:22:06 +0300

    Добавлен абстрактный класс требующийся по условия задачи

---
+++ HomeWorckPaymentGateway/PaymentProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckPaymentGateway
+{
+    public abstract class PaymentProcessor
+    {
+        private string _processorName;
+        private decimal _feePercent;
+
+        public PaymentProcessor(string name, decimal feePercent)
+        {
+            _processorName = name;
+            _feePercent = feePercent;
+        }
+    }
+}
