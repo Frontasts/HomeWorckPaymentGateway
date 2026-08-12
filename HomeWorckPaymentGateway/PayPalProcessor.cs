87996cc8da87ac6ac523f0a108b68892b4373c7c
Frontasts <steamfronts0110@gmail.com>
2026-08-12 22:34:50 +0300

    Добавлен класс PayPalProcessor с переопределённым методом ProcessPayment

---
+++ HomeWorckPaymentGateway/PayPalProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckPaymentGateway
+{
+    public class PayPalProcessor : PaymentProcessor
+    {
+        public PayPalProcessor(string name, decimal feePercent): base(name, feePercent)
+        {
+        }
+
+        public override bool ProcessPayment(decimal amount, string email, out string log)
+        {
+            if (string.IsNullOrEmpty(email) || email.Contains("@") == false)
+            {
+                log = "Ошибка: некорректный email PayPal.";
+                return false;
+            }
+
+            decimal total = CalculateTotal(amount);
+
+            log = $"[PayPal] Аккаунт: {email}\n" +
+                  $"         Сумма: {amount} USD\n" +
+                  $"         Комиссия процессора: {GetFeePercent()}%\n" +
+                  $"         Итого к списанию: {total} USD";
+            return true;
+        }
+    }
+}
