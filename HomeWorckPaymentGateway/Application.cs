66dbbaebf7a6928ab1ee14810d7568dc212f1fef
Frontasts <steamfronts0110@gmail.com>
2026-08-12 22:40:03 +0300

    Добавлен конструктор для инициализации процесоров и поля running

--- HomeWorckPaymentGateway/Application.cs
+++ HomeWorckPaymentGateway/Application.cs
@@ -8,6 +8,18 @@ namespace HomeWorckPaymentGateway
 {
     public class Application
     {
+        private PaymentProcessor[] _processors;
+        private bool _running;
+
+        public Application()
+        {
+            _running = true;
+            _processors = new PaymentProcessor[3];
+            _processors[0] = new CreditCardProcessor("Visa / Mastercard", 2.5m);
+            _processors[1] = new PayPalProcessor("PayPal", 3.0m);
+            _processors[2] = new CryptoProcessor("Bitcoin Gateway", 1.5m, "BTC", 65000m, 0.0001m);
+        }
+
         public void Run()
         {
 
